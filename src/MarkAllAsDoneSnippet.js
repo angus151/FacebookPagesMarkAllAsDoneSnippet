8d4fbc64ff362485118fda5d6f67ae3bb036e276
Angus <1538997+angus151@users.noreply.github.com>
2019-06-05 10:06:51 +1000

    Create MarkAllAsDoneSnippet.js

---
+++ src/MarkAllAsDoneSnippet.js
@@ -0,0 +1,9 @@
+(function run() {
+    let all = document.querySelectorAll('[data-tooltip-content="Mark as done"]');
+    if (all.length == 0) return;
+    let a = all[0];
+    setTimeout(() => {
+        a.click();
+        run();
+    }, 250);
+})();
